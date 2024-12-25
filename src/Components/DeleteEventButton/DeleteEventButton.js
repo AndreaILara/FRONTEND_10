34da87fccbda376be3f67f6c8b297ce8ec45a0fc
Andrea <andreainigolara@hotmail.com>
2024-12-26 00:10:27 +0100

    vercelURL

--- src/Components/DeleteEventButton/DeleteEventButton.js
+++ src/Components/DeleteEventButton/DeleteEventButton.js
@@ -5,15 +5,9 @@ import { apiRequest } from '../../Utils/apiRequest';
 
 export const DeleteEventButton = (buttonContainer, eventObject) => {
   const user = JSON.parse(localStorage.getItem('user'));
-
+  //Si el usuario tiene permisoso de administrador, se muestra un botón que permite eliminar eventos
   if (user.role === 'admin') {
     const eventId = eventObject._id;
-
-    // Crear contenedor de botones
-    const buttonGroup = document.createElement('div');
-    buttonGroup.classList.add('button-container');
-
-    // Crear botón de eliminar
     const deleteEventButton = document.createElement('button');
     const deleteImg = document.createElement('img');
     deleteImg.src = '/borrar.png';
@@ -27,20 +21,41 @@ export const DeleteEventButton = (buttonContainer, eventObject) => {
       confirmationBtn.addEventListener('click', () => {
         confirmationBtn.classList.add('loading');
         deleteEvent(event, eventId);
+        //tomo el evento del listener original para poder eliminar la tarjeta facilmente
       });
     });
-
-    // Agregar botones al contenedor
-    buttonGroup.append(deleteEventButton);
-
-    // Si hay otros botones, los puedes añadir aquí
-    // Ejemplo: Agregar botón de editar
-    const editButton = document.createElement('button');
-    editButton.textContent = 'Editar'; // Cambia según el caso
-    editButton.classList.add('edit-btn');
-    buttonGroup.append(editButton);
-
-    // Añadir el grupo de botones al contenedor original
-    buttonContainer.append(buttonGroup);
+    buttonContainer.append(deleteEventButton);
   }
+};
+
+/* Lógica para eliminar el evento */
+const deleteEvent = async (e, eventId) => {
+  const res = await apiRequest({
+    endpoint: 'events',
+    id: eventId,
+    method: 'DELETE',
+  });
+
+  const response = await res.json();
+  //Si sale todo bien, se elimina la tarjeta y se muestra un mensaje
+  if (res.status === 200) {
+    showToast(response.message, 'red');
+    e.target.parentNode.remove();
+    document.querySelector('.modal').remove();
+  } else {
+    //Si no, se informa al usuario del error
+    showToast(response, 'red');
+  }
+};
+
+const warningBanner = () => {
+  const deleteWarningContainer = Modal();
+  deleteWarningContainer.id = 'del-event';
+  const warningBanner = document.createElement('div');
+  warningBanner.innerHTML = `
+  <p>¿Estás seguro de que quieres eliminar el evento?</p>
+  <button class="del-btn">Sí. Eliminar el evento</button>
+    `;
+  deleteWarningContainer.append(warningBanner);
+  return deleteWarningContainer;
 };
