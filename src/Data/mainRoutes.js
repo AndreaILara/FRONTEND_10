d733ae9d9d4192a0a16e6fa308b49afe87777deb
Andrea <andreainigolara@hotmail.com>
2024-12-25 22:39:59 +0100

    vercelURL

--- src/Data/mainRoutes.js
+++ src/Data/mainRoutes.js
@@ -1,4 +1,5 @@
 const localUrl = 'http://localhost:3000/api/v2';
-const vercelUrl = 'https://backend-kappa-rust-60.vercel.app/';
+const vercelUrl = 'https://backend-kappa-rust-60.vercel.app/api/v2';
+
 
 export const mainRoute = vercelUrl; // Usa localUrl durante el desarrollo
