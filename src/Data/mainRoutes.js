7371573aa7c92c4b3dc9d9637e90ef13b73f752e
Andrea <andreainigolara@hotmail.com>
2025-01-25 19:52:32 +0100

    header

--- src/Data/mainRoutes.js
+++ src/Data/mainRoutes.js
@@ -2,4 +2,4 @@ const localUrl = 'http://localhost:3000/api/v2';
 const vercelUrl = 'https://backend-kappa-rust-60.vercel.app/api/v2';
 
 
-export const mainRoute = vercelUrl; 
+export const mainRoute = localUrl; 
